7f9641b90904db7b04eb2eae10f4356af146fb39
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-03-22 13:43:04 +0300

    Init

---
+++ src/test/java/com/example/restaurant_advisor_react/RestaurantAdvisorReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_advisor_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAdvisorReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
