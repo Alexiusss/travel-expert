53f3fd99f26b09fba0b8e05912e51e26044eec51
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-03-23 21:12:01 +0300

    Added base entity with custom ID generator

---
+++ src/main/java/com/example/restaurant_advisor_react/model/BaseEntity.java
@@ -0,0 +1,75 @@
+package com.example.restaurant_advisor_react.model;
+
+import com.example.restaurant_advisor_react.HasId;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.util.ProxyUtils;
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@MappedSuperclass
+//  https://stackoverflow.com/a/6084701/548473
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public abstract class BaseEntity implements Persistable<String>, HasId {
+
+    @Id
+    // https://codeburst.io/spring-boot-rest-microservices-best-practices-2a6e50797115
+    @GeneratedValue(generator = "custom-generator",
+            strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "custom-generator",
+            strategy = "com.example.restaurant_advisor_react.model.id.generator.BaseIdentifierGenerator")
+    protected String id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    protected Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at")
+    protected Instant modifiedAt;
+
+    @Column(name = "version")
+    @Version
+    protected int version;
+
+    // doesn't work for hibernate lazy proxy
+    public String id() {
+        Assert.notNull(id, "Entity must have id");
+        return id;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+
+    //    https://stackoverflow.com/questions/1638723
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(ProxyUtils.getUserClass(o))) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id.hashCode();
+    }
+}
