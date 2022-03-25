3a56e7d31b633a5674b97341d7ecf57f749c442f
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-03-25 11:12:58 +0300

    Added user controller

---
+++ src/main/java/com/example/restaurant_advisor_react/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.restaurant_advisor_react.controller;
+
+import com.example.restaurant_advisor_react.model.User;
+import com.example.restaurant_advisor_react.servise.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(path = UserController.URL, produces = APPLICATION_JSON_VALUE)
+public class UserController {
+    static final String URL = "/api/v1/users";
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> addUser(User user) {
+        User savedUser = userService.addUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+}
