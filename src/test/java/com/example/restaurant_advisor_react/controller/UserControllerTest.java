ba91af1034f11aa4ea9bb2dd22561b0e54a2d16b
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-04-18 22:19:39 +0300

    Added tests into UserControllerTest

--- src/test/java/com/example/restaurant_advisor_react/controller/UserControllerTest.java
+++ src/test/java/com/example/restaurant_advisor_react/controller/UserControllerTest.java
@@ -1,16 +1,42 @@
 package com.example.restaurant_advisor_react.controller;
 
+import com.example.restaurant_advisor_react.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static com.example.restaurant_advisor_react.controller.UserController.REST_URL;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static com.example.restaurant_advisor_react.util.UserTestData.ADMIN_ID;
+import static com.example.restaurant_advisor_react.util.UserTestData.USER_ID;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserControllerTest extends AbstractControllerTest {
 
+    private static final String REST_URL = UserController.REST_URL + "/";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE));
+    }
+
     @Test
     void get() throws Exception {
-       perform(MockMvcRequestBuilders.get(REST_URL))
-               .andDo(print());
+        perform(MockMvcRequestBuilders.get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE));
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + USER_ID))
+                .andExpect(status().isNoContent());
+        assertFalse(userRepository.findById(USER_ID).isPresent());
     }
 }
