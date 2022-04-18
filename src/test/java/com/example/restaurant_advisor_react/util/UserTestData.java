aa0eb1e740081a06c700efd9a90348dea73f532a
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-04-18 22:17:54 +0300

    Added UserTestData class

---
+++ src/test/java/com/example/restaurant_advisor_react/util/UserTestData.java
@@ -0,0 +1,10 @@
+package com.example.restaurant_advisor_react.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserTestData {
+    public static final String ADMIN_ID = "1";
+    public static final String MODER_ID = "2";
+    public static final String USER_ID = "3";
+}
