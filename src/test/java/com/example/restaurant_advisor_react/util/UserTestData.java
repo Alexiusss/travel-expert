2bff23993e6d3cce57c7d075c47b272f70fc4b04
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-04-20 19:05:32 +0300

    Removed initialization dates from user test data and replaced the date with the current date in the SQL init file.

--- src/test/java/com/example/restaurant_advisor_react/util/UserTestData.java
+++ src/test/java/com/example/restaurant_advisor_react/util/UserTestData.java
@@ -17,9 +17,9 @@ public class UserTestData {
     public static final String MODER_ID = "2";
     public static final String USER_ID = "3";
 
-    public static final User ADMIN = new User(ADMIN_ID, Timestamp.valueOf("2022-04-05 21:36:19.543515").toInstant(), null, 1, "admin@gmail.com", "Admin", "AdminLast", "1", true, null, List.of(Role.ADMIN, Role.MODERATOR, Role.USER));
-    public static final User MODER = new User(MODER_ID, Timestamp.valueOf("2022-04-05 21:36:19.543515").toInstant(), null, 1, "moder@gmail.com", "Moder", "ModerLast", "1", true, null, List.of(Role.MODERATOR, Role.USER));
-    public static final User USER = new User(USER_ID, Timestamp.valueOf("2022-04-05 21:36:19.543515").toInstant(), null, 1, "user@gmail.com", "User", "UserLast", "1", true, null, Collections.singleton(Role.USER));
+    public static final User ADMIN = new User(ADMIN_ID, null, null, 0, "admin@gmail.com", "Admin", "AdminLast", "1", true, null, List.of(Role.ADMIN, Role.MODERATOR, Role.USER));
+    public static final User MODER = new User(MODER_ID, null, null, 0, "moder@gmail.com", "Moder", "ModerLast", "1", true, null, List.of(Role.MODERATOR, Role.USER));
+    public static final User USER = new User(USER_ID, null, null, 0, "user@gmail.com", "User", "UserLast", "1", true, null, Collections.singleton(Role.USER));
 
     public static final List<User> USER_LIST = List.of(ADMIN, MODER, USER);
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,9 +4,9 @@ DELETE
 FROM users;
 
 INSERT INTO users (id, created_at, modified_at, version, activation_code, enabled, email, first_name, last_name, password)
-VALUES ('1', TIMESTAMP '2022-04-05 21:36:19.543515', null, 1, null, true, 'admin@gmail.com', 'Admin', 'AdminLast','1'),
-       ('2', TIMESTAMP '2022-04-05 21:36:19.543515', null, 1, null, true, 'moder@gmail.com', 'Moder', 'ModerLast','1'),
-       ('3', TIMESTAMP '2022-04-05 21:36:19.543515', null, 1, null, true, 'user@gmail.com', 'User', 'UserLast','1');
+VALUES ('1', NOW(), NOW(), 0, null, true, 'admin@gmail.com', 'Admin', 'AdminLast','1'),
+       ('2', NOW(), NOW(), 0, null, true, 'moder@gmail.com', 'Moder', 'ModerLast','1'),
+       ('3', NOW(), NOW(), 0, null, true, 'user@gmail.com', 'User', 'UserLast','1');
 
 INSERT INTO user_role (role, user_id)
 VALUES ('ADMIN', 1),
