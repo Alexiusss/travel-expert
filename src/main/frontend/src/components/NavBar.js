c067ca9631691364f643e2f954ccb9cf09846209
Alexey Boyarinov <kuzma.prutckoff@yandex.ru>
2022-04-28 21:00:27 +0300

    Added internalization for NavBar component

--- src/main/frontend/src/components/NavBar.js
+++ src/main/frontend/src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import {USERS_ROUTE} from "../utils/consts";
 import {Link} from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const NavBar = () => {
+    const {t, i18n} = useTranslation();
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+    }
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -12,9 +21,21 @@ const NavBar = () => {
                 <div className="navbar-nav mr-auto">
                     <li className="nav-item">
                         <Link to={USERS_ROUTE} className="nav-link">
-                            Users
+                            {t("users")}
                         </Link>
                     </li>
+                </div>
+                <div className="navbar-nav ms-auto">
+                    <FormControl sx={{ m: 1, minWidth: 80}}>
+                        <InputLabel>{t("lang")}</InputLabel>
+                        <Select
+                            defaultValue={"en"}
+                            autoWidth
+                        >
+                            <MenuItem value={"en"} onClick={() => changeLanguage("en")}>English</MenuItem>
+                            <MenuItem value={"ru"} onClick={() => changeLanguage("ru")}>Русский</MenuItem>
+                        </Select>
+                    </FormControl>
                 </div>
             </nav>
         </div>
