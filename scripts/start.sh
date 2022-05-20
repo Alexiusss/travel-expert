19905ab05ef4ac3825d0874669e0a3417e4d1f1b
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-05-20 22:30:37 +0300

    Added starting script for app

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mvn clean package -DskipTests
+
+scp target/restaurant_advisor_react-0.0.1-SNAPSHOT.jar \
+    docker/back-end/
+
+cd docker
+
+docker-compose build
+docker-compose up
