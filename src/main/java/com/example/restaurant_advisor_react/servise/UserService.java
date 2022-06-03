06bcff8b92dfc81ae1a60c97651e3a661615e670
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-06-03 22:15:39 +0300

    Implemented UserDetailsService in UserService

--- src/main/java/com/example/restaurant_advisor_react/servise/UserService.java
+++ src/main/java/com/example/restaurant_advisor_react/servise/UserService.java
@@ -1,10 +1,15 @@
 package com.example.restaurant_advisor_react.servise;
 
+import com.example.restaurant_advisor_react.AuthUser;
 import com.example.restaurant_advisor_react.model.User;
 import com.example.restaurant_advisor_react.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
@@ -12,7 +17,8 @@ import org.springframework.util.ObjectUtils;
 import java.util.Optional;
 
 @Service
-public class UserService {
+@Slf4j
+public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
@@ -52,5 +58,13 @@ public class UserService {
 
     public Page<User> findAllPaginated(Pageable pageable) {
         return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.debug("Authenticating '{}'", email);
+       return new AuthUser(userRepository.findByEmailIgnoreCase(email.toLowerCase()).orElseThrow(
+               () -> new UsernameNotFoundException("User '" + email + "' was not found")
+       ));
     }
 }
