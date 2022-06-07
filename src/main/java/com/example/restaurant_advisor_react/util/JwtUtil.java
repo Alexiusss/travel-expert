c3231c16503a904695c3fd6ab56d13887244527f
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-06-07 21:59:08 +0300

    Added token generation methods to the jwt util

--- src/main/java/com/example/restaurant_advisor_react/util/JwtUtil.java
+++ src/main/java/com/example/restaurant_advisor_react/util/JwtUtil.java
@@ -2,21 +2,33 @@ package com.example.restaurant_advisor_react.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import lombok.experimental.UtilityClass;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
-@UtilityClass
+@Component
 public class JwtUtil {
 
+    private static String secret;
+
+    private static long jwtExpirationMs;
+
+    //https://mkyong.com/spring/spring-inject-a-value-into-static-variables/
     @Value("${jwt.secret}")
-    private String secret;
+    private void setSecret(String jwtSecret) {
+        secret = jwtSecret;
+    }
 
     @Value("${jwt.ExpirationMs}")
-    private int jwtExpirationMs;
+    private void setExpiration(long expiration) {
+        jwtExpirationMs = expiration;
+    }
 
     public static <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
@@ -32,13 +44,28 @@ public class JwtUtil {
         return (userName.equals(userDetails.getUsername()) && isTokenExpired(token));
     }
 
-    public Date getExpirationDateFromToken(String token) {
+    public static Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
 
     private static Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
+    }
+
+    public static String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+
+    private static String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
     }
 
     public static String getUserEmailFromToken(String token) {
