b14b50fe4c15919bb60157bb6294f01f376dbd12
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-06-15 20:05:08 +0300

    Added redux store and user slice

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from "react";
+import React from "react";
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,15 +1,19 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from "react-redux";
+import {store} from "./store";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import './i18n';
 
 ReactDOM.render(
     <React.StrictMode>
+        <Provider store={store}>
         <Suspense fallback={<div>Loading...</div>}>
             <App/>
         </Suspense>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
