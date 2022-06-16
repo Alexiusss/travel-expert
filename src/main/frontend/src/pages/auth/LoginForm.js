7b78b22a652652500ec33f363930823ac1cdd4d0
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-06-16 22:26:36 +0300

    Added dispatching for current user

--- src/main/frontend/src/pages/auth/LoginForm.js
+++ src/main/frontend/src/pages/auth/LoginForm.js
@@ -1,13 +1,18 @@
 import React, {useState} from 'react';
-import authService from '../../services/AuthService.js'
+import {useDispatch} from 'react-redux';
+import {useHistory} from "react-router-dom";
+import {setUser} from 'store/slices/userSlice'
+import authService from 'services/AuthService.js'
 import classes from './LoginForm.module.css'
+import MyNotification from "components/UI/notification/MyNotification";
 import {useTranslation} from "react-i18next";
-import MyNotification from "../../components/UI/notification/MyNotification";
-import {getLocalizedErrorMessages} from "../../utils/consts";
+import {getLocalizedErrorMessages} from "utils/consts";
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const {push} = useHistory();
     const [alert, setAlert] = useState({open: false, message: '', severity: 'info'})
     const {t} = useTranslation();
 
@@ -19,8 +24,13 @@ const LoginForm = () => {
         e.preventDefault()
         authService.login(email, password)
             .then(response => {
-                localStorage.setItem('access-token', response.data.accessToken)
-            })
+                dispatch(setUser({
+                email: response.data.email,
+                id: response.data.userId,
+                token: response.data.accessToken,
+            }));
+                push('/');
+           })
             .catch(error =>
                 openAlert(getLocalizedErrorMessages(error.response.data.message), "error")
             )
