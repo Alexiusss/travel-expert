bdd71467898438d074d9079f8979171c9d1a0507
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-06-19 21:37:30 +0300

    Substituted header token instead of cache in jwt filter

--- src/main/java/com/example/restaurant_advisor_react/filter/JwtFilter.java
+++ src/main/java/com/example/restaurant_advisor_react/filter/JwtFilter.java
@@ -3,21 +3,22 @@ package com.example.restaurant_advisor_react.filter;
 import com.example.restaurant_advisor_react.servise.UserService;
 import com.example.restaurant_advisor_react.util.JwtUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
+
+import static com.example.restaurant_advisor_react.util.JwtUtil.validateToken;
 
 @Component
 @AllArgsConstructor
@@ -31,25 +32,17 @@ public class JwtFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
-        if(request.getCookies() == null) {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (!StringUtils.hasText(header) || (StringUtils.hasText(header) && !header.startsWith("Bearer "))) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        Optional<Cookie> jwtOpt = Arrays.stream(request.getCookies())
-                .filter(cookie -> cookie.getName().equals("refresh-token"))
-                .findAny();
-
-        if (jwtOpt.isEmpty()) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        final String token = jwtOpt.get().getValue();
+        final String token = header.split(" ")[1].trim();
 
         UserDetails userDetails = userService.loadUserByUsername(JwtUtil.getUserEmailFromToken(token));
 
-        if (!JwtUtil.validateToken(token, userDetails)) {
+        if (!validateToken(token, userDetails)) {
             filterChain.doFilter(request, response);
             return;
         }
