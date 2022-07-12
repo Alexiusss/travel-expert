72a36874819409bae921799cd794f0a544230821
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-07-12 21:15:11 +0300

    Moved frontend into separate module

--- src/main/java/com/example/restaurant_advisor_react/repository/UserRepository.java
+++ src/main/java/com/example/restaurant_advisor_react/repository/UserRepository.java
@@ -2,16 +2,13 @@ package com.example.restaurant_advisor_react.repository;
 
 import com.example.restaurant_advisor_react.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends BaseRepository<User>{
+public interface UserRepository extends BaseRepository<User> {
 
     @Override
-    @EntityGraph(attributePaths = {"roles"})
+    @EntityGraph(attributePaths = { "roles" })
     List<User> findAll();
 
     Optional<User> findByEmailIgnoreCase(String email);
