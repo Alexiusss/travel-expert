251cf920da7b03fcef29f3c1874120c75c319e50
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-07-16 10:10:23 +0300

    Added support for database migrations using flyway

---
+++ restaurant/src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,16 @@
+create table restaurants (
+                       id varchar(255) not null,
+                       created_at timestamp not null,
+                       modified_at timestamp,
+                       version int4 not null,
+                       name varchar(255),
+                       cuisine varchar(255) not null,
+                       filename varchar(255),
+                       email varchar(255) not null,
+                       address varchar(255) not null,
+                       phone_number varchar(255) not null,
+                       website varchar(255),
+                       primary key (id)
+);
+
+create unique index restaurant_email_unique on restaurants (email);
