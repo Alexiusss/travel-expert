366dedcb7262d004a8f7cc1512ca187a0ca3b731
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-07-29 11:26:59 +0300

    Implemented load verification for restaurant listing component

--- frontend/src/restaurant/RestaurantList.js
+++ frontend/src/restaurant/RestaurantList.js
@@ -2,19 +2,29 @@ import React, {useEffect, useState} from 'react';
 import restaurantService from '../services/RestaurantService'
 import {Container} from "@material-ui/core";
 import ItemGrid from "../components/ItemGrid";
+import { trackPromise, usePromiseTracker } from 'react-promise-tracker';
+import {useTranslation} from "react-i18next";
 
 const RestaurantList = () => {
+    const area = 'restaurants';
+    const { promiseInProgress } = usePromiseTracker({ area });
     const [restaurants, setRestaurants] = useState([]);
 
+    const {t} = useTranslation();
+
     useEffect(() => {
-        restaurantService.getAll().then(({data}) => {
+        trackPromise(
+        restaurantService.getAll(), area).then(({data}) => {
             setRestaurants(data.content)
         });
     }, [setRestaurants])
 
     return (
         <Container>
-            <ItemGrid items={restaurants}/>
+            {promiseInProgress
+                ? <div>{t("data loading")}</div>
+                : <ItemGrid items={restaurants}/>
+            }
         </Container>
     );
 };
