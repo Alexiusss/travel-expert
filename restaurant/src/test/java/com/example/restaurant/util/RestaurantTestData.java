a1b90d07f93f899e76062be2dd435b85c2078dfd
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-08-10 12:02:14 +0300

    Fixed restaurant test data

--- restaurant/src/test/java/com/example/restaurant/util/RestaurantTestData.java
+++ restaurant/src/test/java/com/example/restaurant/util/RestaurantTestData.java
@@ -30,16 +30,16 @@ public class RestaurantTestData {
     public static String NOT_BLANK = "must not be blank";
     public static String DUPLICATE_EMAIL = "Duplicate email";
 
-    public static final Restaurant RESTAURANT1 = new Restaurant(RESTAURANT1_ID, null, null, 0, "restaurant1", "restaurant1 cuisine", null, "restaurant1@gmail.com", "restaurant1 address", "+1111111111", "restaurant1.com");
-    public static final Restaurant RESTAURANT2 = new Restaurant(RESTAURANT2_ID, null, null, 0, "restaurant2", "restaurant2 cuisine", null, "restaurant2@gmail.com", "restaurant2 address", "+2222222222", "restaurant2.com");
-    public static final Restaurant RESTAURANT3 = new Restaurant(RESTAURANT3_ID, RESTAURANT_INSTANT, RESTAURANT_INSTANT, 0, "restaurant3", "restaurant3 cuisine", null, "restaurant3@gmail.com", "restaurant3 address", "+3333333333", "restaurant3.com");
+    public static final Restaurant RESTAURANT1 = new Restaurant(RESTAURANT1_ID, null, null, 0, "restaurant1", "restaurant1 cuisine", null, "restaurant1@gmail.com", "restaurant1 address", "+1 (111) 111-11-11", "restaurant1.com");
+    public static final Restaurant RESTAURANT2 = new Restaurant(RESTAURANT2_ID, null, null, 0, "restaurant2", "restaurant2 cuisine", null, "restaurant2@gmail.com", "restaurant2 address", "+2 (222) 222-22-22", "restaurant2.com");
+    public static final Restaurant RESTAURANT3 = new Restaurant(RESTAURANT3_ID, RESTAURANT_INSTANT, RESTAURANT_INSTANT, 0, "restaurant3", "restaurant3 cuisine", null, "restaurant3@gmail.com", "restaurant3 address", "+3 (333) 333-33-33", "restaurant3.com");
 
     public static Restaurant getNew() {
-        return new Restaurant(null, null, null, 0, "new restaurant", "new restaurant cuisine", null, "new_estaurant@gmail.com", "new restaurant address", "+444444444", "new restaurant.com");
+        return new Restaurant(null, null, null, 0, "new restaurant", "new restaurant cuisine", null, "new_estaurant@gmail.com", "new restaurant address", "+1 (234) 567-89-10", "new restaurant.com");
     }
 
     public static Restaurant getUpdated() {
-        return  new Restaurant(RESTAURANT2_ID, null, null, 0, "updated restaurant2", "updated cuisine", null, "restaurant2@gmail.com", "updated restaurant2 address", "+2222222222", "updated_restaurant2.com");
+        return  new Restaurant(RESTAURANT2_ID, null, null, 0, "updated restaurant2", "updated cuisine", null, "restaurant2@gmail.com", "updated restaurant2 address", "+2 (222) 222-22-22", "updated_restaurant2.com");
     }
 
     public static void stubAdminAuth() {
--- restaurant/src/test/resources/data.sql
+++ restaurant/src/test/resources/data.sql
@@ -2,6 +2,6 @@ DELETE
 FROM restaurants;
 
 INSERT INTO  restaurants (id, created_at, modified_at, version, name, cuisine, filename, email, address, phone_number, website)
-VALUES ('1', NOW(), NOW(), 0, 'restaurant1', 'restaurant1 cuisine', null, 'restaurant1@gmail.com', 'restaurant1 address', '+1111111111', 'restaurant1.com'),
-       ('2', NOW(), NOW(), 0, 'restaurant2', 'restaurant2 cuisine', null, 'restaurant2@gmail.com', 'restaurant2 address', '+2222222222', 'restaurant2.com'),
-       ('3', '2022-07-25 10:39:59.711012', '2022-07-25 10:39:59.711012', 0, 'restaurant3', 'restaurant3 cuisine', null, 'restaurant3@gmail.com', 'restaurant3 address', '+3333333333', 'restaurant3.com');
+VALUES ('1', NOW(), NOW(), 0, 'restaurant1', 'restaurant1 cuisine', null, 'restaurant1@gmail.com', 'restaurant1 address', '+1 (111) 111-11-11', 'restaurant1.com'),
+       ('2', NOW(), NOW(), 0, 'restaurant2', 'restaurant2 cuisine', null, 'restaurant2@gmail.com', 'restaurant2 address', '+2 (222) 222-22-22', 'restaurant2.com'),
+       ('3', '2022-07-25 10:39:59.711012', '2022-07-25 10:39:59.711012', 0, 'restaurant3', 'restaurant3 cuisine', null, 'restaurant3@gmail.com', 'restaurant3 address', '+3 (333) 333-33-33', 'restaurant3.com');
