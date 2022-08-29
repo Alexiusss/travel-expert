0af580c7737d35d249cfd4cc8623fcaace403ca2
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-08-29 21:13:41 +0300

    Added support for tracker promise when fetching review list

--- frontend/src/pages/review/ReviewsSection.jsx
+++ frontend/src/pages/review/ReviewsSection.jsx
@@ -1,22 +1,34 @@
 import React, {useEffect, useState} from 'react';
-
 import reviewService from "../../services/ReviewService";
 import ReviewList from "./ReviewList";
+import {trackPromise, usePromiseTracker} from "react-promise-tracker";
+import {Container} from "@material-ui/core";
+import SkeletonCard from "./SceletonCard";
+
+
 
 const ReviewsSection = (props) => {
+    const area = 'reviews';
+    const {promiseInProgress} = usePromiseTracker({area});
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        reviewService.getAll()
+        trackPromise(
+            reviewService.getAll(), area)
             .then(({data}) => {
-                setReviews(data.content)
-            })
-            .catch(console.error)
+                setReviews(data.content);
+            });
     }, [setReviews])
 
     return (
-        <ReviewList reviews={reviews}/>
-    );
+        <Container>
+            {promiseInProgress
+                ? <SkeletonCard listsToRender={10}/>
+                : <ReviewList reviews={reviews}/>
+            }
+        </Container>
+    )
+        ;
 };
 
 export default ReviewsSection;
