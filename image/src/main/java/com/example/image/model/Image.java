83a97e391cb92c4b975af3848af3dfe83c29cd44
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-09-07 20:18:49 +0300

    Added image model and custom ID generator

---
+++ image/src/main/java/com/example/image/model/Image.java
@@ -0,0 +1,46 @@
+package com.example.image.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(generator = "custom-generator",
+            strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "custom-generator",
+           strategy = "com.example.image.model.id.generator.BaseIdentifierGenerator")
+    private String id;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant modifiedAt;
+
+    @Version
+    private int version;
+
+    private String fileName;
+
+    private String fileType;
+
+    private long size;
+
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] data;
+}
