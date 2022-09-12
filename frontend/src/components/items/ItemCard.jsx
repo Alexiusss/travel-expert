a12a622601199c77ffde9a0e1cfb28a4b0136582
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-09-12 12:28:52 +0300

    Item Card moved to a separate component

---
+++ frontend/src/components/items/ItemCard.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+    Grid,
+    Card,
+    CardContent,
+    Typography,
+    CardActionArea,
+    CardMedia
+} from "@material-ui/core";
+import {Link} from "react-router-dom";
+import MyButton from "../UI/button/MyButton";
+import {useAuth} from "../hooks/UseAuth";
+import {useTranslation} from "react-i18next";
+
+
+const ItemCard = (props) => {
+    const {item, route} = props;
+    const {isAdmin} = useAuth();
+    const {t} = useTranslation();
+
+    return (
+        <Grid item xs={12} sm={6} md={3} key={item.id}>
+            <Link
+                to={{
+                    pathname: `${route}${item.name.replaceAll(" ", "_")}`,
+                    // https://stackoverflow.com/a/63876129
+                    state: {id: item.id}
+                }}
+                className="nav-link"
+            >
+                <Card>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image="http://localhost:8083/api/v1/images/rest2.jpg"
+                            alt={item.name}
+                        />
+
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div">
+                                {item.name}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    {isAdmin ?
+                        <div style={{float: 'right'}}>
+                            <MyButton className="btn btn-outline-info btn-sm" onClick={(e) =>
+                                props.update(e, item)
+                            }>
+                                {t("edit")}
+                            </MyButton>
+                            {' '}
+                            <MyButton className="btn btn-outline-danger btn-sm"
+                                      onClick={e => props.remove(e, item)}>
+                                {t("delete")}</MyButton>
+                        </div>
+                        :
+                        ""
+                    }
+                </Card>
+            </Link>
+
+        </Grid>
+    );
+};
+
+export default ItemCard;
--- frontend/src/components/items/ItemGrid.js
+++ frontend/src/components/items/ItemGrid.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import {
-    Grid,
-    Card,
-    CardContent,
-    Typography,
-    CardActionArea,
-    CardMedia, IconButton
-} from "@material-ui/core";
-import {Link} from "react-router-dom";
-import {useTranslation} from "react-i18next";
-import MyButton from "../UI/button/MyButton";
-import {useAuth} from "../hooks/UseAuth";
+import ItemCard from "./ItemCard";
+import {Grid} from "@material-ui/core";
+
 
 const ItemGrid = (props) => {
-
-    const {isAdmin} = useAuth();
-    const {t} = useTranslation();
 
     const removeItem = (e, item) => {
         e.preventDefault();
@@ -33,49 +21,7 @@ const ItemGrid = (props) => {
               direction="row"
         >
             {props.items.map((item) =>
-                <Grid item xs={12} sm={6} md={3} key={item.id}>
-                    <Link
-                        to={{
-                            pathname: `${props.route}${item.name.replaceAll(" ", "_")}`,
-                            // https://stackoverflow.com/a/63876129
-                            state: {id: item.id}
-                        }}
-                        className="nav-link"
-                    >
-                        <Card>
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="140"
-                                    image="https://www.wilkinslinen.com/website/wp-content/uploads/2016/11/Depositphotos_2099183_m-2015-1.jpg"
-                                    alt={item.name}
-                                />
-
-                                <CardContent>
-                                    <Typography gutterBottom variant="h6" component="div">
-                                        {item.name}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            {isAdmin ?
-                                    <div style={{float: 'right'}}>
-                                        <MyButton className="btn btn-outline-info btn-sm" onClick={(e) =>
-                                            updateItem(e, item)
-                                        }>
-                                            {t("edit")}
-                                        </MyButton>
-                                        {' '}
-                                        <MyButton className="btn btn-outline-danger btn-sm"
-                                                  onClick={e => removeItem(e, item)}>
-                                            {t("delete")}</MyButton>
-                                    </div>
-                                :
-                                ""
-                            }
-                        </Card>
-                    </Link>
-
-                </Grid>
+                <ItemCard item={item} route={props.route} remove={removeItem} update={updateItem} key={item.id}/>
             )}
         </Grid>
     );
