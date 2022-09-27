c2f98a8a3c4f6b5914722b0e8f9e1386beab52a6
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-09-27 12:38:23 +0300

    Fixed item rating component for UI

--- frontend/src/components/items/ItemRating.jsx
+++ frontend/src/components/items/ItemRating.jsx
@@ -1,7 +1,5 @@
-import React, {useEffect} from 'react';
-import {Box, Checkbox, FormControlLabel, FormLabel, LinearProgress} from "@material-ui/core";
-import {FormGroup} from "reactstrap";
-import FormControl from "@material-ui/core/FormControl";
+import React from 'react';
+import {Box, Checkbox, FormControlLabel, Grid, LinearProgress, Typography} from "@material-ui/core";
 import {useTranslation} from "react-i18next";
 
 const ItemRating = (props) => {
@@ -15,24 +13,25 @@ const ItemRating = (props) => {
     };
 
     return (
-        <Box sx={{display: 'flex'}}>
-            <FormControl sx={{m: 5}} component="fieldset" variant="standard">
-                <FormLabel component="legend">Traveler rating</FormLabel>
-                <FormGroup>
-                    {Object.entries(props.rating.ratingsMap).map(([key, value]) =>
-                        <Box key={key} sx={{ml: 3}}>
+            <Box>
+            <Grid container >
+                {Object.entries(props.rating.ratingsMap).map(([key, value]) =>
+                    <Grid key={key} container alignItems="center" direction="row">
+                        <Grid item  xs={4}>
                             <FormControlLabel
                                 control={
                                     <Checkbox checked={false} size="small" color="default"/>
                                 }
                                 label={t(labels[key])}
                             />
+                        </Grid>
+                        <Grid item xs={4}>
                             <LinearProgress variant="determinate" value={100 - (100 / key)}/>
-                        </Box>
-                    )
-                    }
-                </FormGroup>
-            </FormControl>
+                        </Grid>
+                    </Grid>
+                )
+                }
+            </Grid>
         </Box>
     );
 };
