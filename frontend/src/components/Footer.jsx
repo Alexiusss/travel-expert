4f186e6bd7f584aaeb9829e98da113a07575e7f5
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-10-21 21:27:51 +0300

    Moved menu for changing language from NavBar to Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,34 @@
+import React from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import {useTranslation} from "react-i18next";
 const Footer = () => {
+    const {t, i18n} = useTranslation();
+
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+    }
+
     return (
         <footer className="text-center text-lg-start bg-light text-muted">
             <div className="text-center p-4" >
-                <div className="container">
+                <div className="container centered" >
                     © {new Date().getFullYear()} Copyright Text
                     <a href="#!">
                          GitHub
                     </a>
+                    <FormControl sx={{ m: 1, minWidth: 80}}>
+                        <InputLabel>{t("lang")}</InputLabel>
+                        <Select
+                            defaultValue={"en"}
+                            autoWidth
+                        >
+                            <MenuItem value={"en"} onClick={() => changeLanguage("en")}>English</MenuItem>
+                            <MenuItem value={"ru"} onClick={() => changeLanguage("ru")}>Русский</MenuItem>
+                        </Select>
+                    </FormControl>
                 </div>
             </div>
         </footer>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,20 +3,13 @@ import {useDispatch} from 'react-redux'
 import {PROFILE, RESTAURANTS_ROUTE, REVIEWS_ROUTE, USERS_ROUTE} from "../utils/consts";
 import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
 import {useAuth} from "./hooks/UseAuth";
 import MyButton from "./UI/button/MyButton";
 import authService from "services/AuthService"
 import {removeUser} from "../store/slices/userSlice";
 
 const NavBar = () => {
-    const {t, i18n} = useTranslation();
-    const changeLanguage = (language) => {
-        i18n.changeLanguage(language);
-    }
+    const {t} = useTranslation();
     const {isAuth, isAdmin, isModerator, authUserId} = useAuth();
     const dispatch = useDispatch();
 
@@ -34,63 +27,45 @@ const NavBar = () => {
                 < Link to="/" className="navbar-brand">
                     Restaurant advisor
                 </Link>
-                <div className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to={RESTAURANTS_ROUTE} className="nav-link">
-                        {t("restaurants")}
-                    </Link>
-                    </li>
-                </div>
-                <div className="navbar-nav mr-auto">
-                    {isAdmin || isModerator ?
-                        <li className="nav-item">
-                            <Link to={USERS_ROUTE} className="nav-link">
-                                {t("users")}
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="container-fluid">
+                        <Nav.Item>
+                            <Link to={RESTAURANTS_ROUTE} className="nav-link">
+                                {t("restaurants")}
                             </Link>
-                        </li>
-                        :
-                        ""
-                    }
-                </div>
-                <div className="navbar-nav mr-auto">
-                    {isAdmin || isModerator ?
-                        <li className="nav-item">
-                            <Link to={REVIEWS_ROUTE} className="nav-link">
-                                {t("reviews")}
-                            </Link>
-                        </li>
-                        :
-                        ""
-                    }
-                </div>
-                <div className="navbar-nav mr-auto">
-                    {isAuth?
-                        <li className="nav-item">
-                            <Link  className="nav-link"
-                                to={{
-                                pathname: PROFILE,
-                                state: {authorId: authUserId}
-                            }}>
-                                {t("profile")}
-                            </Link>
-                        </li>
-                        :
-                        ""
-                    }
-                </div>
-                <div className="navbar-nav ms-auto">
-                    <FormControl sx={{m: 1, minWidth: 80}}>
-                        <InputLabel>{t("lang")}</InputLabel>
-                        <Select
-                            defaultValue={"en"}
-                            autoWidth
-                        >
-                            <MenuItem value={"en"} onClick={() => changeLanguage("en")}>English</MenuItem>
-                            <MenuItem value={"ru"} onClick={() => changeLanguage("ru")}>Русский</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <div className="navbar-nav mr-auto">
-                        <li className="nav-item">
+                        </Nav.Item>
+                        {isAdmin || isModerator ?
+                            <>
+                                <Nav.Item>
+                                    <Link to={USERS_ROUTE} className="nav-link">
+                                        {t("users")}
+                                    </Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Link to={REVIEWS_ROUTE} className="nav-link">
+                                        {t("reviews")}
+                                    </Link>
+                                </Nav.Item>
+                            </>
+                            :
+                            null
+                        }
+                        {isAuth
+                            ?
+                            <Nav.Item>
+                                <Link className="nav-link"
+                                      to={{
+                                          pathname: PROFILE,
+                                          state: {authorId: authUserId}
+                                      }}>
+                                    {t("profile")}
+                                </Link>
+                            </Nav.Item>
+                            :
+                            null
+                        }
+                        <Nav.Item className="ms-auto  ">
                             {isAuth ?
                                 <MyButton style={{marginTop: 10}} className={"btn btn-outline-primary ml-2 btn-sm"}
                                           onClick={() => logout()}>
