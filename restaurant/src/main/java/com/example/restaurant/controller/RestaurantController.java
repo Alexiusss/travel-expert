fd57827ed4c52cad1f2d088669d3277c77bfa71f
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-12-01 21:42:22 +0300

    Added OpenAPI for restaurant microservice

--- restaurant/src/main/java/com/example/restaurant/controller/RestaurantController.java
+++ restaurant/src/main/java/com/example/restaurant/controller/RestaurantController.java
@@ -2,6 +2,8 @@ package com.example.restaurant.controller;
 
 import com.example.restaurant.model.Restaurant;
 import com.example.restaurant.service.RestaurantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -25,18 +27,21 @@ public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
+    @Operation(summary = "Get a restaurant by its id")
     @GetMapping("/{id}")
     public ResponseEntity<Restaurant> get(@PathVariable String id) {
         log.info("get restaurant {}", id);
         return ResponseEntity.ok(restaurantService.get(id));
     }
 
+    @Operation(summary = "Get a restaurant by its name")
     @GetMapping("/{name}/name")
     public ResponseEntity<Restaurant> getByName(@PathVariable String name) {
         log.info("get restaurant {}", name);
         return ResponseEntity.ok(restaurantService.getByName(name));
     }
 
+    @Operation(summary = "Return a list of restaurants and filtered according the query parameters")
     @GetMapping
     public Page<Restaurant> getAll(
             @RequestParam(defaultValue = "0") int page,
@@ -47,6 +52,8 @@ public class RestaurantController {
         return restaurantService.findAllPaginated(PageRequest.of(page, size), filter);
     }
 
+    @Operation(summary = "Create a new restaurant", description = "A JWT token is required to access this API")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping
     public ResponseEntity<Restaurant> create(@RequestHeader(name = "Authorization", defaultValue = "empty") String authorization, @Valid @RequestBody Restaurant restaurant) {
 
@@ -64,6 +71,8 @@ public class RestaurantController {
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
+    @Operation(summary = "Delete a restaurant by its id", description = "A JWT token is required to access this API")
+    @SecurityRequirement(name = "Bearer Authentication")
     @DeleteMapping("/{id}")
     public ResponseEntity<Restaurant> delete(@RequestHeader(name = "Authorization", defaultValue = "empty") String authorization, @PathVariable String id) {
         log.info("delete restaurant{}", id);
@@ -75,6 +84,8 @@ public class RestaurantController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Update a restaurant by its id", description = "A JWT token is required to access this API")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@RequestHeader(name = "Authorization", defaultValue = "empty") String authorization,
                                     @Valid @RequestBody Restaurant restaurant, @PathVariable String id) {
