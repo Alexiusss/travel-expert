254055a559706f6935544be9acc844833e95186a
Alexey Boyarinov <boyarinovalexey@gmail.com>
2022-12-06 19:13:17 +0300

    Disabled ProfilePopover mapping for the current user in SubList for UI

--- frontend/src/pages/review/ReviewItem.jsx
+++ frontend/src/pages/review/ReviewItem.jsx
@@ -31,7 +31,7 @@ const ReviewItem = (props) => {
         like = Function.prototype
     } = props;
     const author = props.author || props.item.author;
-    const isAuthor = authUserId === author.authorId;
+    const isAuthor = authUserId === author.authorId || author.id;
     const [reviewsCount, setReviewsCount] = useState(props.reviewsCount || author.reviewsCount);
     const isAuthUserLiked = likes.includes(authUserId);
     const likesCount = likes.length || 0;
@@ -57,7 +57,7 @@ const ReviewItem = (props) => {
 
     return (
         <div>
-            {Boolean(anchorEl) &&
+            {(Boolean(anchorEl) && !isAuthor) &&
                 <ProfilePopover anchorEl={anchorEl} setAnchorEl={setAnchorEl} {...author} rating={userRating}/>
             }
             <Card className="paperItem" elevation={3}
