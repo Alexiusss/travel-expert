aaab98c684068750e43b9e3f93b4e0b00e0b9da7
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-01-16 13:01:01 +0300

    Added hotel model and custom ID generator

---
+++ hotel/src/main/java/com/example/hotel/model/Hotel.java
@@ -0,0 +1,45 @@
+package com.example.hotel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Hotel {
+
+    @Id
+    @GeneratedValue(generator = "custom-generator",
+            strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "custom-generator",
+            strategy = "com.example.hotel.model.id.generator.BaseIdentifierGenerator")
+    private String id;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant modifiedAt;
+
+    @Version
+    private int version;
+
+    private String name;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String website;
+
+    private Integer hotelClass;
+}
