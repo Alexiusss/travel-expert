a053d998d5822247faaebc60e31c7a0099dcc716
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-01-17 12:50:05 +0300

    Added test for controller

---
+++ hotel/src/test/java/com/example/hotel/controller/HotelControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.hotel.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@Transactional
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class HotelControllerTest {
+
+    private static final String REST_URL = HotelController.REST_URL + "/";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ResultActions perform(MockHttpServletRequestBuilder builder) throws Exception {
+        return mockMvc.perform(builder);
+    }
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
