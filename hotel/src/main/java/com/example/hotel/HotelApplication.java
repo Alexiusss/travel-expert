64e6e79c6214611beb831c06fa2ac2fc0c92971f
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-01-20 14:39:12 +0300

    Added feign client to hotel

--- hotel/src/main/java/com/example/hotel/HotelApplication.java
+++ hotel/src/main/java/com/example/hotel/HotelApplication.java
@@ -2,8 +2,14 @@ package com.example.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(
+        basePackages = "com.example.clients"
+)
 public class HotelApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelApplication.class, args);
--- hotel/src/main/java/com/example/hotel/config/HotelConfig.java
+++ hotel/src/main/java/com/example/hotel/config/HotelConfig.java
@@ -3,12 +3,21 @@ package com.example.hotel.config;
 import com.example.common.util.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HotelConfig {
     @Autowired
     private void setMapper(ObjectMapper objectMapper) {
         JsonUtil.setMapper(objectMapper);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
