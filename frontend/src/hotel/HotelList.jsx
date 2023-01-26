736ffdd0e9cbe9f5c47325656d6abaf53f87ae58
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-01-26 14:56:46 +0300

    Added pagination for the restaurant list

--- frontend/src/hotel/HotelList.jsx
+++ frontend/src/hotel/HotelList.jsx
@@ -5,24 +5,48 @@ import {HOTELS_ROUTE} from "../utils/consts";
 import hotelService from "../services/HotelService"
 import SkeletonGrid from "../components/SkeletonGrid";
 import {trackPromise, usePromiseTracker} from "react-promise-tracker";
+import Pagination from "@material-ui/lab/Pagination";
+import MySelect from "../components/UI/select/MySelect";
 
 const HotelList = () => {
     const area = 'hotels';
     const {promiseInProgress} = usePromiseTracker({area});
     const [hotels, setHotels] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(0);
+    const pageSizes = [12, 36, 96];
 
     useEffect(() => {
         trackPromise(
-            hotelService.getAll(), area).then(({data}) => {
-            setHotels(data);
+            hotelService.getAll(size, page), area).then(({data}) => {
+            setHotels(data.content);
+            setTotalPages(data.totalPages)
         });
-    }, [setHotels]);
+    }, [setHotels, page, size]);
+
+    const changePage = (e, value) => {
+        setPage(value);
+    }
+
+    const changeSize = (event) => {
+        setSize(event.target.value);
+        setPage(1);
+    }
 
     return (
         <Container>
+            <MySelect size={size} changeSize={changeSize} pageSizes={pageSizes}/>
             {promiseInProgress
                 ? <SkeletonGrid listsToRender={16}/>
-                : <ItemGrid items={hotels} route={HOTELS_ROUTE}/>
+                : <>
+                    <ItemGrid items={hotels} route={HOTELS_ROUTE}/>
+                    {totalPages > 0 ?
+                        <Pagination count={totalPages} page={page} onChange={changePage} shape="rounded"
+                                    className="pagination"/>
+                        : null
+                    }
+                </>
             }
         </Container>
 
--- frontend/src/services/HotelService.js
+++ frontend/src/services/HotelService.js
@@ -1,8 +1,14 @@
 import $api from "../http/http-common";
 import {HOTELS_ROUTE} from "../utils/consts";
 
-const getAll = () => {
-    return $api.get(HOTELS_ROUTE);
+const getAll = (size = 20, page = 1, filter = "") => {
+    return $api.get(HOTELS_ROUTE, {
+            params: {
+                size: size,
+                page: page - 1,
+                filter: filter
+            },
+        });
 };
 
 export default {getAll}
