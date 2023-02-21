dd23651f67ebeade364dae96a7b6b48b43cd78ca
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-02-21 15:41:31 +0300

    The adjustment for the number of columns depends on the screen width for the ItemImages component of the UI

--- frontend/src/components/items/ItemImages.jsx
+++ frontend/src/components/items/ItemImages.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Box, Button, ImageList, ImageListItem, ImageListItemBar} from "@material-ui/core";
+import React, {useEffect, useRef, useState} from 'react';
+import {Button, ImageList, ImageListItem, ImageListItemBar} from "@material-ui/core";
 import {API_URL} from "../../http/http-common";
 import {IMAGE_ROUTE} from "../../utils/consts";
 import {useAuth} from "../hooks/UseAuth";
@@ -8,32 +8,41 @@ import {useAuth} from "../hooks/UseAuth";
 const ItemImages = (props) => {
     const {isAdmin, isModerator} = useAuth();
     const {images = [], itemId, removeImage = Function.prototype} = props;
+    const componentRef = useRef(null);
+    const [columnsCount, setColumnsCount] = useState(1);
+    const overFlowY = (images.length > (3 / columnsCount)) ? 'scroll' : 'hidden';
+
+    // https://stackoverflow.com/a/56011277
+    useEffect(() => {
+        setColumnsCount(Math.round(componentRef.current.offsetWidth / 410))
+    }, [componentRef.current]);
 
     return (
-        <Box sx={{height: 190, overflow: 'scroll' }}>
-        <ImageList
-             cols={3}
-        >
-            {
-                images.map((image, index) =>
-                    <ImageListItem key={index}>
-                        <img
-                            src={API_URL + IMAGE_ROUTE + `${image}`}
-                            alt="Image"
-                            loading="lazy"
-                        />
-                        {(isAdmin || isModerator) &&
-                            <ImageListItemBar
-                                actionIcon={<Button size="small" color="secondary">X</Button>}
-                                position='top'
-                                onClick={() => removeImage(image, itemId)}
+
+         <div ref={componentRef} style={{height: 182, overflowY: {overFlowY}, overflowX: 'hidden'}}>
+            <ImageList
+                cols={columnsCount}
+            >
+                {
+                    images.map((image, index) =>
+                        <ImageListItem key={index} >
+                            <img
+                                src={API_URL + IMAGE_ROUTE + `${image}`}
+                                alt="Image"
+                                loading="lazy"
                             />
-                        }
-                    </ImageListItem>
-                )
-            }
-        </ImageList>
-        </Box>
+                            {(isAdmin || isModerator) &&
+                                <ImageListItemBar
+                                    actionIcon={<Button size="small" color="secondary">X</Button>}
+                                    position='top'
+                                    onClick={() => removeImage(image, itemId)}
+                                />
+                            }
+                        </ImageListItem>
+                    )
+                }
+            </ImageList>
+        </div>
     );
 };
 
