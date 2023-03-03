c0f12df7d6109b6b43eb1756c898d9831e4d1ad0
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-03-03 18:28:46 +0300

    Added exception handling for duplicate emails for restaurant

--- restaurant/src/main/java/com/example/restaurant/controller/RestaurantExceptionHandler.java
+++ restaurant/src/main/java/com/example/restaurant/controller/RestaurantExceptionHandler.java
@@ -1,12 +1,37 @@
 package com.example.restaurant.controller;
 
 import com.example.common.GlobalExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import static com.example.common.util.ValidationUtil.getRootCause;
+import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.STACK_TRACE;
 
 @RestControllerAdvice
+@Slf4j
 public class RestaurantExceptionHandler extends GlobalExceptionHandler {
+
+    public static final String EXCEPTION_DUPLICATE_EMAIL = "Duplicate email";
+
     public RestaurantExceptionHandler(ErrorAttributes errorAttributes) {
         super(errorAttributes);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> conflict(WebRequest request, DataIntegrityViolationException ex) {
+        String rootMsg = getRootCause(ex).getMessage();
+        if (rootMsg.toLowerCase().contains("restaurant_email_unique")) {
+            log.warn("Conflict at request  {}: {}", request, rootMsg);
+            return createResponseEntity(request, ErrorAttributeOptions.of(), EXCEPTION_DUPLICATE_EMAIL, HttpStatus.CONFLICT);
+        }
+        log.error("Conflict at request " + request, getRootCause(ex));
+        return createResponseEntity(request, ErrorAttributeOptions.of(STACK_TRACE), rootMsg, HttpStatus.CONFLICT);
     }
 }
