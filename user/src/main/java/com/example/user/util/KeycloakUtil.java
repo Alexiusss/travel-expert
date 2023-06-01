fd468cc4caeeb379d168c24d1ad917aa4a7458c3
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-01 17:27:44 +0300

    Added KeycloakUtil

---
+++ user/src/main/java/com/example/user/util/KeycloakUtil.java
@@ -0,0 +1,68 @@
+package com.example.user.util;
+
+import com.example.user.model.User;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+
+@Service
+public class KeycloakUtil {
+
+    @Value("${keycloak.auth-server-url}")
+    private String serverUrl;
+    @Value("${keycloak.realm}")
+    private String realm;
+    @Value("${keycloak.resource}")
+    private String clientId;
+    @Value("${keycloak.credentials.secret}")
+    private String clientSecret;
+
+    private static Keycloak keycloak;
+
+    public Keycloak getInstance() {
+        if (keycloak == null) {
+            keycloak = KeycloakBuilder.builder()
+                    .realm(realm)
+                    .serverUrl(serverUrl)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                    .build();
+        }
+        return keycloak;
+    }
+
+    public Response createKeycloakUser(User user) {
+        RealmResource realmResource = getInstance().realm(realm);
+        UsersResource userResource = realmResource.users();
+        CredentialRepresentation credentialRepresentation = createPasswordCredentials(user.getPassword());
+
+        UserRepresentation kcUser = new UserRepresentation();
+        kcUser.setUsername(user.getUsername());
+        kcUser.setCredentials(Collections.singletonList(credentialRepresentation));
+        kcUser.setEmail(user.getEmail());
+        kcUser.setFirstName(user.getFirstName());
+        kcUser.setLastName(user.getLastName());
+        kcUser.setEnabled(true);
+        kcUser.setEmailVerified(false);
+
+        return userResource.create(kcUser);
+    }
+
+    private CredentialRepresentation createPasswordCredentials(String password) {
+        CredentialRepresentation passwordCredentials = new CredentialRepresentation();
+        passwordCredentials.setTemporary(false);
+        passwordCredentials.setType(CredentialRepresentation.PASSWORD);
+        passwordCredentials.setValue(password);
+        return passwordCredentials;
+    }
+}
