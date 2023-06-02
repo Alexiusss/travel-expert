eed6f12f488e62024d877994cbeb343dbb8fc634
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-02 17:34:48 +0300

    Added CRUD operations to KeycloakUtil

--- user/src/main/java/com/example/user/util/KeycloakUtil.java
+++ user/src/main/java/com/example/user/util/KeycloakUtil.java
@@ -1,18 +1,23 @@
 package com.example.user.util;
 
-import com.example.user.model.User;
+import com.example.user.model.dto.UserDTO;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Service
 public class KeycloakUtil {
@@ -27,8 +32,11 @@ public class KeycloakUtil {
     private String clientSecret;
 
     private static Keycloak keycloak;
+    private static RealmResource realmResource;
+    private static UsersResource usersResource;
 
-    public Keycloak getInstance() {
+    @PostConstruct
+    public Keycloak intKeycloak() {
         if (keycloak == null) {
             keycloak = KeycloakBuilder.builder()
                     .realm(realm)
@@ -37,13 +45,44 @@ public class KeycloakUtil {
                     .clientSecret(clientSecret)
                     .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
                     .build();
+            realmResource = keycloak.realm(realm);
+            usersResource = realmResource.users();
         }
         return keycloak;
     }
 
-    public Response createKeycloakUser(User user) {
-        RealmResource realmResource = getInstance().realm(realm);
-        UsersResource userResource = realmResource.users();
+    public Response createKeycloakUser(UserDTO user) {
+        return usersResource.create(createUserRepresentation(user));
+    }
+
+    public void addRoles(String userId, List<String> roles) {
+        List<RoleRepresentation> kcRoles = new ArrayList<>();
+        roles.forEach(role -> {
+            RoleRepresentation roleRep = realmResource.roles().get(role).toRepresentation();
+            kcRoles.add(roleRep);
+        });
+        UserResource uniqueUserResource = usersResource.get(userId);
+        uniqueUserResource.roles().realmLevel().add(kcRoles);
+    }
+
+    public void updateKeycloakUser(UserDTO user, String userId) {
+        UserResource userResource = usersResource.get(userId);
+        userResource.update(createUserRepresentation(user));
+    }
+
+    public UserRepresentation findUserById(String userId) {
+        return usersResource.get(userId).toRepresentation();
+    }
+
+    public List<UserRepresentation> searchKeycloakUsers(String text) {
+        return usersResource.searchByAttributes(text);
+    }
+
+    public void deleteKeycloakUser(String userId) {
+        usersResource.get(userId).remove();
+    }
+
+    private UserRepresentation createUserRepresentation(UserDTO user) {
         CredentialRepresentation credentialRepresentation = createPasswordCredentials(user.getPassword());
 
         UserRepresentation kcUser = new UserRepresentation();
@@ -52,10 +91,9 @@ public class KeycloakUtil {
         kcUser.setEmail(user.getEmail());
         kcUser.setFirstName(user.getFirstName());
         kcUser.setLastName(user.getLastName());
-        kcUser.setEnabled(true);
-        kcUser.setEmailVerified(false);
-
-        return userResource.create(kcUser);
+        kcUser.setEnabled(user.isEnabled());
+        kcUser.setEmailVerified(user.getActivationCode() == null);
+        return kcUser;
     }
 
     private CredentialRepresentation createPasswordCredentials(String password) {
