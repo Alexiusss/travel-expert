1ce36995bd2104c6577383c0dde415d95c8899a8
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-02 17:32:31 +0300

    Added UserDTO

---
+++ user/src/main/java/com/example/user/model/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package com.example.user.model.dto;
+
+import com.example.common.HasIdAndEmail;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.util.StringUtils;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+public class UserDTO implements HasIdAndEmail {
+
+    private String id;
+
+    @Email
+    @NotBlank
+    @Size(min = 5, max = 128)
+    private String email;
+
+    @NotBlank
+    @Size(min = 5, max = 128)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 128)
+    private String lastName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank
+    @Size(min = 5, max = 128)
+    private String password;
+
+    @NotBlank
+    @Size(min = 5, max = 128)
+    private String username;
+
+    private boolean enabled;
+
+    private String activationCode;
+
+    public void setEmail(String email) {
+        this.email = StringUtils.hasText(email) ? email.toLowerCase() : null;
+    }
+}
