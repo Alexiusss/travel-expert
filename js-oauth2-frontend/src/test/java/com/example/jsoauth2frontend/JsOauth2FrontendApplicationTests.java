92dcaac1725c08da8903a5fb25531261b62a7ddd
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-04 17:26:45 +0300

    Added js-oauth2-frontend module

---
+++ js-oauth2-frontend/src/test/java/com/example/jsoauth2frontend/JsOauth2FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoauth2frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsOauth2FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
