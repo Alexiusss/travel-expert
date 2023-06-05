e80804b2379ebe555603585820cf6c42f9b1bc1b
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-05 20:06:47 +0300

    Added functionality to request auth code for oauth2-frontend

--- oauth2-frontend/src/main/resources/static/js/oauth2.js
+++ oauth2-frontend/src/main/resources/static/js/oauth2.js
@@ -1,16 +1,22 @@
 // https://www.rfc-editor.org/rfc/rfc7636
 
 const SHA_256 = "SHA-256";
+const KEYCLOAK_URI = "http://localhost:8180/realms/travel-expert-realm/protocol/openid-connect";
+const RESPONSE_TYPE_CODE = "code";
+const CLIENT_ID = "travel-expert-client";
+const SCOPE = "openid";
+const S256 = "S256";
+const AUTH_CODE_REDIRECT_URI = "http://localhost:9191/redirect";
 
 function initValues() {
     let state = generateState(30);
     document.getElementById("originalState").innerHTML = state;
-    
+
     let codeVerifier = generateCodeVerifier();
     document.getElementById("codeVerifier").innerHTML = codeVerifier;
 
     generateCodeChallenge(codeVerifier).then(codeChallenge => {
-        console.log("codeChallenge = " + codeChallenge)
+        requestAuthCode(state, codeChallenge);
     })
 }
 
@@ -42,4 +48,18 @@ function base64UrlEncode(sourceValue) {
     let base64Encoded = btoa(stringValue);
     let base64UrlEncoded = base64Encoded.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
     return base64UrlEncoded;
+}
+
+function requestAuthCode(state, codeChallenge) {
+    let authUrl = KEYCLOAK_URI + "/auth";
+
+    authUrl += "?response_type=" + RESPONSE_TYPE_CODE;
+    authUrl += "&client_id=" + CLIENT_ID;
+    authUrl += "&state=" + state;
+    authUrl += "&scope=" + SCOPE;
+    authUrl += "&code_challenge=" + codeChallenge;
+    authUrl += "&code_challenge_method=" + S256;
+    authUrl += "&redirect_uri=" + AUTH_CODE_REDIRECT_URI;
+
+    window.open(authUrl, 'auth window', 'width=800,height=600,left=350,top=200')
 }
