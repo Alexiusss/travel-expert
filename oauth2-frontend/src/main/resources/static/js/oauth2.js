919532af32d6356c10dd264a9d3a15594d32af84
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-08 16:42:10 +0300

    Added JWT decoding functionality to oauth2.js

--- oauth2-frontend/src/main/resources/static/js/oauth2.js
+++ oauth2-frontend/src/main/resources/static/js/oauth2.js
@@ -151,6 +151,9 @@ function accessTokenResponse(data, status, jqXHR) {
     console.log("refresh_token = " + refreshToken);
     console.log("id_token = " + idToken);
 
+    let payload = getJsonPayload(idToken);
+    document.getElementById("email").innerHTML = "Hi " + payload["email"];
+
     localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
     localStorage.setItem(ID_TOKEN_KEY, idToken);
 
@@ -228,4 +231,15 @@ function logout() {
 
     accessToken = "";
     refreshToken = ""
+}
+
+// https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+function getJsonPayload(token) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
