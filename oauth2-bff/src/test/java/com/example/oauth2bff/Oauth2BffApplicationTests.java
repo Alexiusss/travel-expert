fe05208d24e6e988f3b3f1e9a59f191808952f90
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-06-09 17:30:50 +0300

    Added oauth2-bff module

---
+++ oauth2-bff/src/test/java/com/example/oauth2bff/Oauth2BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2BffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
