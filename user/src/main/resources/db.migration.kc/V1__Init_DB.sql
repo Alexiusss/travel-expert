e8a97e2a59ebc95d901f716d8abdf39265a3a8f1
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-07-27 17:33:29 +0300

    Fixed constraint for subscribers init SQL script for kc profile

--- user/src/main/resources/db.migration.kc/V1__Init_DB.sql
+++ user/src/main/resources/db.migration.kc/V1__Init_DB.sql
@@ -2,6 +2,5 @@ create table subscriptions
 (
     channel_id    varchar(255) not null,
     subscriber_id varchar(255) not null,
-    primary key (channel_id, subscriber_id),
-    constraint subscribers_unique unique (channel_id, subscriber_id)
+    primary key (channel_id, subscriber_id)
 )
