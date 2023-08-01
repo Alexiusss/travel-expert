9b89a58a64c294a503932534245a8553296461ec
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-08-01 21:38:55 +0300

    Added KeycloakTestConfig

---
+++ user/src/test/java/com/example/user/KeycloakTestConfig.java
@@ -0,0 +1,29 @@
+package com.example.user;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test_kc")
+@TestConfiguration
+public class KeycloakTestConfig {
+    @Bean
+    public Keycloak keycloak(
+            @Value("${keycloak.auth-server-url}") String url,
+            @Value("${keycloak.realm}") String realm,
+            @Value("${keycloak.resource}") String clientId,
+            @Value("${keycloak.credentials.secret}") String clientSecret
+    ) {
+        return KeycloakBuilder.builder()
+                .serverUrl(url)
+                .realm(realm)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
+}
