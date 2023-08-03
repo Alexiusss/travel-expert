b52efe3e7c3fbfa87322e901516af0fa9ba69677
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-08-03 21:16:20 +0300

    Added test data to KcUserTestData and to sql initial script

--- user/src/test/java/com/example/user/util/kc/KcUserTestData.java
+++ user/src/test/java/com/example/user/util/kc/KcUserTestData.java
@@ -1,21 +1,41 @@
 package com.example.user.util.kc;
 
-import dasniko.testcontainers.keycloak.KeycloakContainer;
+import com.example.common.util.JsonUtil;
+import com.example.common.util.MatcherFactory;
+import com.example.user.model.User;
+import com.example.user.model.dto.UserDTO;
 import lombok.experimental.UtilityClass;
 import org.keycloak.admin.client.Keycloak;
+
+import java.util.List;
 
 @UtilityClass
 public class KcUserTestData {
 
+    public static final MatcherFactory.Matcher<UserDTO> USER_DTO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(UserDTO.class, "password");
+
+    public static final String ADMIN_ID = "1";
+    public static final String MODER_ID = "2";
+    public static final String USER_ID = "3";
+    public static final String ADMIN_MAIL = "admin@gmail.com";
+    public static final String MODER_MAIL = "moder@gmail.com";
+    public static final String USER_MAIL = "user@gmail.com";
+    public static final String USERNAME = "username";
+
+    public static final UserDTO ADMIN_DTO = new UserDTO(ADMIN_ID, ADMIN_MAIL, "Admin", "AdminLast", "adminPassword", "adminusername", true, "adminFileName", List.of("ADMIN", "MODERATOR", "USER"));
+    public static final UserDTO MODER_DTO = new UserDTO(MODER_ID, MODER_MAIL, "Moder", "ModerLast", "moderPassword", "moderusername", true, "moderFileName", List.of("MODERATOR", "USER"));
+    public static final UserDTO USER_DTO = new UserDTO(USER_ID, USER_MAIL, "User", "UserLast", "userPassword", USERNAME, true, "userFileName", List.of("USER"));
+
     public static String getKeycloakToken(Keycloak keycloakAdminClient) {
-            String access_token = keycloakAdminClient.tokenManager().getAccessToken().getToken();
-            return "Bearer " + access_token;
+        String access_token = keycloakAdminClient.tokenManager().getAccessToken().getToken();
+        return "Bearer " + access_token;
     }
 
-    public static void changeKeycloakPorts(KeycloakContainer keycloak) {
-        int port = keycloak.getMappedPort(8080);
-        String url = "http://localhost:" + port;
-        System.setProperty("keycloak.auth-server-url", url);
-        System.setProperty("spring.security.oauth2.resourceserver.jwt.jwk-set-uri", url + "/realms/travel-expert-realm/protocol/openid-connect/certs");
+    public static UserDTO getNewUser() {
+        return new UserDTO(null, "new@gmail.com", "new name", "new lastname", "newpassword", "newusername", true, "new filename", List.of("USER"));
+    }
+
+    public static String jsonWithPassword(UserDTO user, String passw) {
+        return JsonUtil.writeAdditionProps(user, "password", passw);
     }
 }
--- user/src/test/resources/keycloak/test-data.sql
+++ user/src/test/resources/keycloak/test-data.sql
@@ -1,25 +1,8 @@
 DELETE
-FROM user_entity;
-DELETE
-FROM user_role_mapping;
-DELETE
-FROM keycloak_role;
+FROM subscriptions;
 
-INSERT INTO user_entity (id, email, email_constraint, email_verified, enabled, first_name, last_name, realm_id, username, created_timestamp)
-VALUES ('user-id', 'user@test.com', 'user@test.com', true, true, 'User', 'One', 'travel-expert-realm', 'travel-user', 1684924946651),
-       ('moder-id', 'moder@test.com', 'moder@test.com', true, true, 'Moder', 'One', 'travel-expert-realm', 'travel-moder', 1684924946652),
-       ('admin-id', 'admin@test.com', 'admin@test.com', true, true, 'Admin', 'One', 'travel-expert-realm', 'travel-admin', 1684924946653);
-
-
-INSERT INTO user_role_mapping (role_id, user_id)
-VALUES ('user-role-id', 'user-id'),
-       ('user-role-id', 'moder-id'),
-       ('moder-role-id', 'moder-id'),
-       ('user-role-id', 'admin-id'),
-       ('moder-role-id', 'admin-id'),
-       ('admin-role-id', 'admin-id');
-
-INSERT INTO keycloak_role (id, client_realm_constraint, client_role, name, realm_id)
-VALUES ('user-role-id', 'travel-expert-realm', false,  'USER', 'travel-expert-realm'),
-       ('moder-role-id', 'travel-expert-realm', false,  'MODERATOR', 'travel-expert-realm'),
-       ('admin-role-id', 'travel-expert-realm', false,  'ADMIN', 'travel-expert-realm');
+INSERT INTO subscriptions (channel_id, subscriber_id)
+VALUES ('1', '2'),
+       ('1', '3'),
+       ('2', '1'),
+       ('3', '2');
